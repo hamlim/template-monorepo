e189e05b96dd4bd0f2361e1b200dec4682900bee
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-05-16 17:22:50 -0400

    Add not-found and global-error handling

---
+++ apps/docs/app/global-error.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function GlobalError() {
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-6">
+      <div className="space-y-2 text-center">
+        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">Oops, something went wrong!</h1>
+        <p className="text-gray-500 dark:text-gray-400">
+          We apologize for the inconvenience. Please try again later or navigate back to the homepage.
+        </p>
+      </div>
+      <Link
+        className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+        href="/"
+      >
+        Go back home
+      </Link>
+    </main>
+  );
+}
---
+++ apps/template-app/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-6">
+      <div className="space-y-2 text-center">
+        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">Oops! Page not found.</h1>
+        <p className="text-gray-500 dark:text-gray-400">The page you're looking for doesn't exist.</p>
+      </div>
+      <Link
+        className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+        href="/"
+      >
+        Go back home
+      </Link>
+    </main>
+  );
+}
