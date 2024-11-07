050c4b418c1afff08421b62a82bf3ace1034a545
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-11-06 19:59:53 -0500

    Fix global-error styles, upgrade next

--- apps/docs/app/global-error.tsx
+++ apps/docs/app/global-error.tsx
@@ -1,24 +1,77 @@
 "use client";
+import NextImage from "next/image";
 import Link from "next/link";
+import "./globals.css";
+
+export const metadata = {
+  title: "TODO",
+  description: "TODO",
+};
+
+function themeCheck() {
+  let prefersDarkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  let preferred = prefersDarkModeQuery.matches ? "dark" : "light";
+  document.documentElement.classList.add(preferred);
+  prefersDarkModeQuery.addEventListener("change", (e) => {
+    let newPreferred = e.matches ? "dark" : "light";
+    document.documentElement.classList.remove(preferred);
+    document.documentElement.classList.add(newPreferred);
+    preferred = newPreferred;
+  });
+}
 
 export default function GlobalError() {
   return (
-    <main className="flex h-screen w-full flex-col items-center justify-center gap-6">
-      <div className="space-y-2 text-center">
-        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
-          Oops, something went wrong!
-        </h1>
-        <p className="text-gray-500 dark:text-gray-400">
-          We apologize for the inconvenience. Please try again later or navigate
-          back to the homepage.
-        </p>
-      </div>
-      <Link
-        className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-        href="/"
-      >
-        Go back home
-      </Link>
-    </main>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          suppressHydrationWarning
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: need to inject the script contents
+          dangerouslySetInnerHTML={{
+            __html: `(${themeCheck.toString()})()`,
+          }}
+        />
+      </head>
+      <body className="min-h-screen bg-background font-sans antialiased">
+        <main className="min-h-[100vh] min-w-[100vw] flex flex-col">
+          <section className="flex-grow flex items-center justify-center">
+            <div className="space-y-2 text-center">
+              <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
+                Oops, something went wrong!
+              </h1>
+              <p className="text-gray-500 dark:text-gray-400">
+                We apologize for the inconvenience. Please try again later or
+                navigate back to the homepage.
+              </p>
+              <Link href="/">Go back home</Link>
+            </div>
+          </section>
+          <footer className="flex items-center justify-between py-5 px-8">
+            <div>
+              <h3 className="flex items-center mb-2">
+                <NextImage
+                  className="inline-flex mr-2"
+                  width={25}
+                  height={25}
+                  src="/feijoa-logo.png"
+                  alt=""
+                />
+                Feijoa Kit
+              </h3>
+              <p>Built by Forge Labs</p>
+            </div>
+            <p>
+              <a
+                is="a"
+                href="mailto:matthewjameshamlin@gmail.com"
+                className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 [&.state-disabled]:text-gray-400 [&.state-disabled]:cursor-not-allowed text-primary underline-offset-4 hover:underline focus:underline"
+              >
+                Contact Us
+              </a>
+            </p>
+          </footer>
+        </main>
+      </body>
+    </html>
   );
 }
