f7771ae6a1e864a5c8905513d441e9fbadac99d1
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-11-25 20:39:23 -0500

    Remove errant footer in global-error, add theme check logic

--- apps/docs/app/global-error.tsx
+++ apps/docs/app/global-error.tsx
@@ -1,5 +1,4 @@
 "use client";
-import NextImage from "next/image";
 import Link from "next/link";
 import "./globals.css";
 
@@ -46,30 +45,6 @@ export default function GlobalError() {
               <Link href="/">Go back home</Link>
             </div>
           </section>
-          <footer className="flex items-center justify-between py-5 px-8">
-            <div>
-              <h3 className="flex items-center mb-2">
-                <NextImage
-                  className="inline-flex mr-2"
-                  width={25}
-                  height={25}
-                  src="/feijoa-logo.png"
-                  alt=""
-                />
-                Feijoa Kit
-              </h3>
-              <p>Built by Forge Labs</p>
-            </div>
-            <p>
-              <a
-                is="a"
-                href="mailto:matthewjameshamlin@gmail.com"
-                className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 [&.state-disabled]:text-gray-400 [&.state-disabled]:cursor-not-allowed text-primary underline-offset-4 hover:underline focus:underline"
-              >
-                Contact Us
-              </a>
-            </p>
-          </footer>
         </main>
       </body>
     </html>
