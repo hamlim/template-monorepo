48bfac6222efd9c5633aa690a37e1d82187a0215
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-01-19 21:51:45 -0500

    Setup 'docs' theme within the docs app

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,113 +1,239 @@
-import Image from "next/image";
+import { Code } from "bright";
+import { StarIcon } from "lucide-react";
+import type { Metadata } from "next";
+import {
+  Blockquote,
+  H1,
+  H2,
+  InlineCode,
+  Link,
+  P,
+  UnorderedList,
+} from "~/components/typography";
+import { Button } from "~/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+
+export const metadata: Metadata = {
+  title: "One Version - A strict dependency conformance tool for monorepos!",
+  description:
+    "One Version is a strict dependency conformance tool for monorepos, managing dependencies across repos has never been easier!",
+  authors: [
+    {
+      name: "Matt Hamlin",
+      url: "https://matthamlin.me",
+    },
+  ],
+};
+
+Code.theme = {
+  dark: "github-dark",
+  light: "github-light",
+  lightSelector: "html.light",
+  darkSelector: "html.dark",
+};
+
+const sectionClasses =
+  "py-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 max-w-[75ch] mx-auto min-h-[40vh] flex flex-col justify-center";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <header className={sectionClasses}>
+        <H1>One Version</H1>
+        <P>
+          One Version is a strict dependency conformance tool for monorepos,
+          managing dependencies across repos has never been easier!
+        </P>
+        <P>
+          This tool ensures that all workspaces in your monorepo are using the
+          same version of a dependency, and also an (opt-in) strict versioning
+          strategy to ensure that all dependencies are pinned to an exact
+          version.
+        </P>
+
+        <div className="pt-10 flex row justify-evenly items-center">
+          <Button asChild>
+            <a href="#installation">Get Started</a>
+          </Button>
+          <Button variant="outline" asChild>
+            <a href="https://github.com/hamlim/one-version">
+              <StarIcon className="mr-2 inline-flex" /> Star on GitHub
+            </a>
+          </Button>
         </div>
-      </div>
+      </header>
+      <section id="installation" className={sectionClasses}>
+        <H2>Installation</H2>
+        <P>
+          Install <InlineCode>one-version</InlineCode> via your favorite package
+          manager:
+        </P>
+        <div className="mt-6">
+          <Tabs defaultValue="bun">
+            <TabsList>
+              <TabsTrigger value="bun">Bun</TabsTrigger>
+              <TabsTrigger value="yarn">Yarn</TabsTrigger>
+              <TabsTrigger value="pnpm">pnpm</TabsTrigger>
+              <TabsTrigger value="npm">npm</TabsTrigger>
+            </TabsList>
+            <div className="my-10">
+              <TabsContent value="bun">
+                <Code lang="shell">bun install one-version</Code>
+              </TabsContent>
+              <TabsContent value="yarn">
+                <Code lang="shell">yarn add one-version</Code>
+              </TabsContent>
+              <TabsContent value="pnpm">
+                <Code lang="shell">pnpm install one-version</Code>
+              </TabsContent>
+              <TabsContent value="npm">
+                <Code lang="shell">npm install one-version</Code>
+              </TabsContent>
+            </div>
+          </Tabs>
+        </div>
+        <Blockquote>
+          <P>
+            Notably! <InlineCode>one-version</InlineCode> should be installed at
+            the root of your monorepo.
+          </P>
+        </Blockquote>
+      </section>
+      <section className={sectionClasses}>
+        <H2 className="mt-4">Usage:</H2>
+        <P>
+          Once you&apos;ve installed <InlineCode>one-version</InlineCode>, you
+          can add a <InlineCode>one-version:check</InlineCode> script in your
+          root <InlineCode>package.json</InlineCode>:
+        </P>
+        <Code lang="json">
+          {`{
+  "scripts": {
+    "one-version:check": "one-version check"
+  }
+}`}
+        </Code>
+        <P>
+          You can now run <InlineCode>bun run one-version:check</InlineCode> (or
+          use whatever package manager you have setup) to check for version
+          conformance!
+        </P>
+      </section>
+      <section className={sectionClasses}>
+        <H2>Configuration:</H2>
+        <P>
+          <InlineCode>one-version</InlineCode> can work out of the box without
+          any configuration at all, however if you&apos;d like to allow specific
+          workspaces to use different versions of a dependency, you&apos;ll need
+          to add a configuration file.
+        </P>
+        <P>
+          <InlineCode>one-version</InlineCode> currently supports the following
+          package managers:
+        </P>
+        <UnorderedList>
+          <li>
+            <InlineCode>bun</InlineCode>
+          </li>
+          <li>
+            <InlineCode>pnpm</InlineCode>
+          </li>
+          <li>
+            <InlineCode>npm</InlineCode> - Greater than version 7.x
+          </li>
+          <li>
+            <InlineCode>yarn-classic</InlineCode> - Yarn version 1.x
+          </li>
+          <li>
+            <InlineCode>yarn-berry</InlineCode> - Yarn version 2.x, 3.x, 4.x
+          </li>
+          <li>
+            <InlineCode>deno</InlineCode> - When using{" "}
+            <InlineCode>package.json</InlineCode>
+          </li>
+        </UnorderedList>
+        <P>
+          You can configure <InlineCode>one-version</InlineCode> via either a{" "}
+          <InlineCode>one-version.config.jsonc</InlineCode> or{" "}
+          <InlineCode>one-version.config.json</InlineCode> file. An example
+          configuration is provided below:
+        </P>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Code lang="json">
+          {`{
+  "$schema": "https://one-version.vercel.app/schema.json",
+  // one of: "bun", "yarn-berry", "yarn-classic", "pnpm", "npm"
+  // by default it will try to detect the package manager based on the presence of a lockfile
+  "packageManager": "bun",
+  // A mapping of dependencies, and which workspaces are "allowed" to use different versions
+  "overrides": {
+    "react": {
+      "18.0.0": ["pkg-a"],
+      // Wildcards are supported, and will capture any workspaces!
+      "17.0.0": ["*"]
+    }
+  },
+  // one of: "pin", "loose", defaults to "loose" if not provided
+  // pin: all dependencies and devDependencies must use an exact version
+  // meaning no ranges (\`^\`, \`~\`, \`.x\`, etc.) are allowed
+  "versionStrategy": "pin"
+}`}
+        </Code>
+      </section>
+      <section className={sectionClasses}>
+        <H2>Background and Inspiration:</H2>
+        <P>
+          This package is a spiritual fork of the{" "}
+          <Link href="https://github.com/wayfair/one-version">
+            <InlineCode>@wayfair/one-version</InlineCode>
+          </Link>{" "}
+          package which I had contributed to while at Wayfair.
+        </P>
+        <P>
+          That package still works fine, but hasn&apos;t been maintained in some
+          time - and also doesn&apos;t support either{" "}
+          <InlineCode>bun</InlineCode> or <InlineCode>npm</InlineCode>.
+        </P>
+        <P>
+          Both this package and the original implement a version of
+          Google&apos;s <InlineCode>One-Version Rule</InlineCode>:
+        </P>
+        <Blockquote>
+          <P>
+            For every dependency in [a] repository, there must be only one
+            version of that dependency to choose
+            <sup>
+              <Link href="#fn-1">1</Link>
+            </sup>
+          </P>
+        </Blockquote>
+        <P id="fn-1">
+          [1] -{" "}
+          <Link href="https://abseil.io/resources/swe_at_google.2.pdf">
+            Software Engineering At Google
+          </Link>{" "}
+          - Winters, Manshreck and Wright, 2020, p. 341
+        </P>
+      </section>
+      <footer className={sectionClasses}>
+        <P>
+          The source code for the library is available on{" "}
+          <Link href="https://github.com/hamlim/one-version">GitHub</Link>. If
+          you run into any bugs, please report them via{" "}
+          <Link href="https://github.com/hamlim/one-version/issues/new">
+            issues
+          </Link>
+          .
+        </P>
+        <P>
+          If you&apos;d like to discuss changes to the project, feel free to
+          start a{" "}
+          <Link href="https://github.com/hamlim/one-version/discussions/new/choose">
+            discussion
+          </Link>
+          !
+        </P>
+      </footer>
     </main>
   );
 }
---
+++ apps/docs/components/typography.tsx
@@ -0,0 +1,135 @@
+import { cn } from "~/lib/utils";
+
+export function H1(
+  props: React.HTMLAttributes<HTMLHeadingElement> & {
+    children?: React.ReactNode;
+  },
+) {
+  return (
+    <h1
+      {...props}
+      className={cn(
+        props.className,
+        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+      )}
+    />
+  );
+}
+
+export function H2(
+  props: React.HTMLAttributes<HTMLHeadingElement> & {
+    children?: React.ReactNode;
+  },
+) {
+  return (
+    <h2
+      {...props}
+      className={cn(
+        props.className,
+        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+      )}
+    />
+  );
+}
+
+export function H3(
+  props: React.HTMLAttributes<HTMLHeadingElement> & {
+    children?: React.ReactNode;
+  },
+) {
+  return (
+    <h3
+      {...props}
+      className={cn(
+        props.className,
+        "scroll-m-20 text-2xl font-semibold tracking-tight",
+      )}
+    />
+  );
+}
+
+export function H4(
+  props: React.HTMLAttributes<HTMLHeadingElement> & {
+    children?: React.ReactNode;
+  },
+) {
+  return (
+    <h4
+      {...props}
+      className={cn(
+        props.className,
+        "scroll-m-20 text-xl font-semibold tracking-tight",
+      )}
+    />
+  );
+}
+
+export function P(
+  props: React.HTMLAttributes<HTMLParagraphElement> & {
+    children?: React.ReactNode;
+  },
+) {
+  return (
+    <p
+      {...props}
+      className={cn(props.className, "leading-7 [&:not(:first-child)]:mt-6")}
+    />
+  );
+}
+
+export function Blockquote(
+  props: React.HTMLAttributes<HTMLQuoteElement> & {
+    children?: React.ReactNode;
+  },
+) {
+  return (
+    <blockquote
+      {...props}
+      className={cn(props.className, "mt-6 border-l-2 pl-6 italic")}
+    />
+  );
+}
+
+export function UnorderedList(
+  props: React.HTMLAttributes<HTMLUListElement> & {
+    children?: React.ReactNode;
+  },
+) {
+  return (
+    <ul
+      {...props}
+      className={cn(props.className, "my-6 ml-6 list-disc [&>li]:mt-2")}
+    />
+  );
+}
+
+export function InlineCode(
+  props: React.HTMLAttributes<HTMLElement> & { children?: React.ReactNode },
+) {
+  return (
+    <code
+      {...props}
+      className={cn(
+        props.className,
+        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+      )}
+    />
+  );
+}
+
+export function Link(
+  props: React.HTMLAttributes<HTMLAnchorElement> & {
+    children?: React.ReactNode;
+    href: string;
+  },
+) {
+  return (
+    <a
+      {...props}
+      className={cn(
+        props.className,
+        "font-medium text-primary underline underline-offset-4",
+      )}
+    />
+  );
+}
